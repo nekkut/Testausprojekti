5f6ec13e4cd86ac8bb5df749d7dafdb5e2c1ff1b
unknown <aapo.ylaautio@gmail.com>
2020-12-06 13:20:17 +0200

    test description fix

--- tests/isArgument.test.js
+++ tests/isArgument.test.js
@@ -1,7 +1,7 @@
 import isArguments from '../src/isArguments';
 
 describe('Test IsArguments', () => {
-  test('Input function() { return asd }() should produce output true', () => {
+  test('Input function() { return arguments }() should produce output true', () => {
     expect(isArguments(function() { return arguments }())).toBe(true)
   })
   test('Input [1, 2, 3] should produce output false', () => {
