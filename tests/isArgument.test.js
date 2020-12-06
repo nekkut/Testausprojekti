addc57995d4b1b978ad345b48206f6a3daa1ec3c
unknown <aapo.ylaautio@gmail.com>
2020-12-06 13:18:34 +0200

    typo fix

--- tests/isArgument.test.js
+++ tests/isArgument.test.js
@@ -1,8 +1,8 @@
 import isArguments from '../src/isArguments';
 
 describe('Test IsArguments', () => {
-  test('input function() { return asd }() should produce output true', () => {
-    expect(I(function() { return arguments }())).toBe(true)
+  test('Input function() { return asd }() should produce output true', () => {
+    expect(isArguments(function() { return arguments }())).toBe(true)
   })
   test('Input [1, 2, 3] should produce output false', () => {
     expect(isArguments([1, 2, 3])).toBe(false)
