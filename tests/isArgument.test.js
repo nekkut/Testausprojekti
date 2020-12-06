cb6d705feeb8aeb8e5634a70a37c785f1ff8d0ef
unknown <aapo.ylaautio@gmail.com>
2020-12-06 13:17:05 +0200

    typo error

--- tests/isArgument.test.js
+++ tests/isArgument.test.js
@@ -2,9 +2,9 @@ import isArguments from '../src/isArguments';
 
 describe('Test IsArguments', () => {
   test('input function() { return asd }() should produce output true', () => {
-    expect(isArguments(function() { return arguments }())).toBe(true)
+    expect(I(function() { return arguments }())).toBe(true)
   })
-  test('input [1, 2, 3] should produce output false', () => {
+  test('Input [1, 2, 3] should produce output false', () => {
     expect(isArguments([1, 2, 3])).toBe(false)
   })
 })
